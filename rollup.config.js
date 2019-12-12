768c4c354fe9fc72e76a2208a51c1fcc118f5ed9
久生 <victor.wzp@antfin.com>
2019-12-12 15:37:33 +0800

    feat: 增加 rollup 打包

---
+++ rollup.config.js
@@ -0,0 +1,38 @@
+import babel from 'rollup-plugin-babel'
+import cjs from 'rollup-plugin-commonjs'
+// import { terser } from 'rollup-plugin-terser'
+import resolve from 'rollup-plugin-node-resolve'
+// import typescript from 'rollup-plugin-typescript'
+// import visualizer from 'rollup-plugin-visualizer'
+
+process.env.BABEL_ENV = 'rollup'
+
+const sdkName = 'Form' // 输出的全局变量名称
+
+export default {
+  input: 'src/index',
+  output: {
+    format: 'esm',
+    name: sdkName,
+    file: 'dist/index.js',
+    globals: {
+      react: 'React',
+      'react-dom': 'ReactDOM'
+    }
+  },
+  external: [ 'react', 'react-dom' ],
+  plugins: [
+    resolve(),
+    // typescript(),
+    babel({
+      runtimeHelpers: true,
+      exclude: 'node_modules/**'
+    }),
+    cjs()
+    // terser()
+    // visualizer({
+    //   filename: './dist/statistics.html',
+    //   title: 'My Bundle'
+    // })
+  ]
+}
